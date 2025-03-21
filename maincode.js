ab02e9d7643f25aab52a898af9c6aefa7a3d7d97
Gabe-Antell-Proulx <gzap5573@gmail.com>
2025-03-21 16:01:55 -0700

    Update maincode.js

--- maincode.js
+++ maincode.js
@@ -186,7 +186,11 @@ function loadState() {
      document.getElementById('copier').style.visibility = 'hidden';
      document.getElementById('X_end').style.visibility = 'hidden';
    }
- }
+   for (let butterfly=0; butterfly<4; butterfly++){
+   if (document.getElementById(logger[15+butterfly]).style.backgroundColor = '#33A33C'){
+     document.getElementById("key-" + document.getElementById('sci1').innerHTML).style.backgroundColor = '#33A33C'
+   }
+   }
  else{
    for (let x=0;x<=3;x++){
      document.getElementById(logger_questionPages[x]).innerHTML += "<li>" + logger_prompts[x][0].substr(4, logger_prompts[x][0].length) + "</li>";
